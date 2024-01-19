5baf3cec3716eec1ba2056a48a9db7a659f3c233
ylieumo <yoldjampou@gmail.com>
2024-01-18 21:26:54 -0500

    created the view folder

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
+// Import the Express module
 const express = require("express")
+// Create an instance of the Express application
 const app = express();
+// Import the EJS view engine
+const ejs = require('ejs');
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 const port = 3000;
+
+
+// Use the custom middleware for all routes
+app.use(middleware)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
 
-// Define a route for the home page
+// // Define a route for the home page
+
 app.get("/", (req, res) =>{
-    res.send("Welcome to our family home page!");
+    res.render('index');
 });
-// Define a route for /about
+
+// // Define a route for /about 
 app.get("/about", (req, res) =>{
-    res.send("This page is about us.");
+    res.render(" about");
 });
 
-app.get("/user", (req, res) =>{
-    res.send("This is  our individual user page. ");
-});
-
- // Define a route with multiple parameters
 app.get("/user/:userName", (req, res) => {
-    res.send(`This page will bring you t to the user page for: ${req.params.userName}.`);
+    res.render(`userName`);
   });
 
-app.get("/user/:userID/activities", (req, res) => {
-    res.send(`This page will direct you to the user activities page` );
+  app.get("/blogs/create", (req, res) =>{
+    res.render('create')
+  })
+
+  app.use((req, res) =>{
+    res.status(404).render( '404' )
   });
 
-  app.get("/user/:userName/activities/:data", (req, res) => {
-    res.send(
-      `This page will direct you to the user activities pagee for ${req.params.userName}, and her favorite is ${req.params.data} for the family.`
-    );
-  });
 
-
-
-
-
+    
+  // Custom middleware function
+  function middleware (req, res, next) {
+    console.log('The page wxiste');
+    next(); // Call the next middleware in the stack
+  };
 
  
